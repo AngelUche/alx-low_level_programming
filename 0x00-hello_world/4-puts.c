2f4124a6344872f9f56beffb8b48c5adefea3422
AngelUche <ucheorji9974@gmail.com>
2022-06-29 15:32:06 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Begining of code
+ * main - Entry point
  *
- * Return: Always 0 (if its succesful)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
