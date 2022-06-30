404a5baa8c061056451b00337386acfe5679fc2a
AngelUche <ucheorji9974@gmail.com>
2022-06-30 14:23:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quote, 59);
+	write(2, quot, 59);
 	return (1);
 }
