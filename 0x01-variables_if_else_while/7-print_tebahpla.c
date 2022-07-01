a83c6c808b432031a407126bbef814840892c6d1
AngelUche <ucheorji9974@gmail.com>
2022-07-01 13:44:58 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet im reverse oreder
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
