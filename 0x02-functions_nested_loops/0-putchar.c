fd5606affab677a30c662b1e2ad54db2e4eb3b59
AngelUche <ucheorji9974@gmail.com>
2022-07-05 04:44:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,5 @@
+
+#include <main.h>
 #include <stdio.h>
 
 /**
@@ -9,7 +11,16 @@ int main(void)
 
 
 	{
-printf("_putchar\n");
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
 return (0);
 
 }
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This program prints the result of each task that was given on more nested loop.This readme is a step guide on each project task.
