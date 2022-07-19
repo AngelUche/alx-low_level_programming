45c48f3b61f85d4d6a90b7be8e6660c9ccee1fa1
AngelUche <ucheorji9974@gmail.com>
2022-07-19 00:24:03 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,16 +8,11 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, m = 0;
-
-	for (i = 0; i < 64; i++)
-	{
-		if (i % 8 == 0 && i != 0)
-		{
-			m = i;
-			_putchar('\n');
-		}
-		_putchar(a[i / 8][i - m]);
-	}
-	_putchar('\n');
+int i, j;
+for(i = 0: a[i][7]; i++)
+{
+for(j = 0; j < 8; j++);
+_putchar(a[i][j]);
+_putchar('\n');
+}
 }
