7819318f3f55029ea40eb0467d0b336f2e0d635a
AngelUche <ucheorji9974@gmail.com>
2022-07-28 12:52:04 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,7 @@
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
 {
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -23,7 +24,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	ptr = malloc(new_size);
 	if (new_size == old_size)
 		return (ptr);
-		free(ptr);
+
+	free(ptr);
 	ptr = malloc(new_size);
+
 	return (ptr);
 }
