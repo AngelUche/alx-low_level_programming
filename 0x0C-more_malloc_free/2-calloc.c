b0dcf60a58ce3a9ad1b13c624511eccb379ff7c5
AngelUche <ucheorji9974@gmail.com>
2022-07-28 09:01:12 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
-	for (a = 0; b < (nmemb * size); a++)
+	for (a = 0; a < (nmemb * size); a++)
 			s[a] = 0;
 			return (s);
 }
