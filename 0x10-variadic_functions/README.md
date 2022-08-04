970a985cc6c84225cc16386c0b3f1438a1f59dcc
AngelUche <ucheorji9974@gmail.com>
2022-08-04 12:57:17 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+# 0x10. C - Variadic functions
+This function will introduce you to how to create a printf function like in c.
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+
+int sum_them_all(const unsigned int n, ...);
+#endif
