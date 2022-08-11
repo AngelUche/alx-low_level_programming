d5a31bbe4da8f702adc5b9639ad0797e8b7832bc
AngelUche <ucheorji9974@gmail.com>
2022-08-11 11:12:11 -0700

    Task 0 of linked list

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+#0x12. C - Singly linked lists
+the concept of linked list is explicitly explsined using this project. Each task exposes you to different topics in single linked list.
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+#endif
