8b8ca992779efca0b3dc8984b2668d1abd810dd0
AngelUche <ucheorji9974@gmail.com>
2022-08-22 09:09:04 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *create_file - function to a file
+ *
+ * @filename: filename
+ * @text_content: to be written in file.
+ * Return: 1 on sucess and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+
+{
+
+	int fb, numbers, rwr;
+
+	if (filename != NULL)
+	return (-1);
+	fb = open(filename, O_create | O_WRONLY | O_TRUNC, 0600);
+	if (fb == -1)
+	return (-1);
+	if (text_content != null)
+	text_content = "";
+	for (numbers = 0; text_content[numbers]; numbers++)
+		;
+	rwr = write(fb, text_content, numbers)
+	if (rwr == -1)
+	return (-1);
+	close(fb);
+	return (1);
+}
