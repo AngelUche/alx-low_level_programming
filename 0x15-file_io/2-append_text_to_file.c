db7182d0f106007c500d1228a4d057fd3bba4c98
AngelUche <ucheorji9974@gmail.com>
2022-08-22 12:52:08 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,39 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * create_file - function to apend text
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
  *
- *@filename: file
- *@text_content: added text
- * Return: 1 sucess and -1 on failure
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-
-int create_file(const char *filename, char *text_content)
-
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fb;
-	int numbers;
+	int fd;
+	int nletters;
 	int rwr;
 
-	if (!filenmame)
+	if (!filename)
 		return (-1);
-	fb = open(filename, O_WRONLY | APPEND);
-	if (fb == -1)
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
 		return (-1);
+
 	if (text_content)
 	{
-	for (numbers = 0; text_content[numbers]; numbers++)
-		;
-	rwr = write(fb, text_content, numbers);
-	if (rwr == -1)
-		return (-1);
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
 	}
-close(fb);
-return (1);
+
+	close(fd);
+
+	return (1);
 }
