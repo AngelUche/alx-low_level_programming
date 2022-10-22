56e5a7fa4eb17d4311413352786d2b4784139d9a
AngelUche <106807725+AngelUche@users.noreply.github.com>
2022-10-22 01:31:20 +0000

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,27 +1,29 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_get - function that retrieves a value associated with a key
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
  *
- *@ht: hash table you want to look into
- *@key: the key you are looking for
- *
- *Return: the value associated with the element else NULL
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *nodo;
-unsigned long int index;
+	hash_node_t *node;
+	unsigned long int index;
 
-if (key == NULL || ht == NULL)
-return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-index = key_index((const unsigned char *)key, ht->size);
-nodo = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
-for (; nodo != NULL; nodo = nodo->next)
-{
-if (strcmp(nodo->key, key) == 0)
-return (nodo->value);
-}
-return (NULL);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
