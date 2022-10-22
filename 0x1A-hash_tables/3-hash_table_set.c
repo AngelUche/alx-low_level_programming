74fee5a7d9d5650d6af28bf71a82fb4bd359bef7
AngelUche <106807725+AngelUche@users.noreply.github.com>
2022-10-22 01:30:15 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,36 +1,53 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_set - adds an element to the hash table
- *@ht: pointer to the hash
- *@key: pointer to the key
- *@value: value to add
- *Return: 1 if is success or 0
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long index, size;
-hash_node_t *new_node;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-if (ht  == NULL || key == NULL || value == NULL)
-return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-size = ht->size;
-index = key_index((const unsigned char *)key, size);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
-{
-ht->array[index]->value = strdup(value);
-return (1);
-}
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 
-new_node = malloc(sizeof(hash_node_t));
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-if (new_node == NULL)
-return (0);
-
-new_node->key = strdup(key);
-new_node->value = strdup(value);
-new_node->next = ht->array[index];
-ht->array[index] = new_node;
-return (1);
+	return (1);
 }
